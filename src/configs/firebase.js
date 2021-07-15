0d9cc3af8143c023bfd74c5735bb4862022f6275
andrei1311 <andrei.agrigoroaie@gmail.com>
2021-07-15 13:11:28 +0300

    new site

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,11 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCcbZMkwd4ZV_1DiH0R3TZFCg9GKaIMeoM",
-    authDomain: "react-sirluggia-shop.firebaseapp.com",
-    databaseURL: "https://react-sirluggia-shop.firebaseio.com",
-    projectId: "react-sirluggia-shop",
-    storageBucket: "react-sirluggia-shop.appspot.com",
-    messagingSenderId: "184253035461",
-    appId: "1:184253035461:web:20fe5c173c7675c74c51ac"
-};
+    apiKey: "AIzaSyCJ9yD58fDUxyvSIotiywHuMfFIdgS_Wjo",
+    authDomain: "lumea-machetelor-ab4ab.firebaseapp.com",
+    projectId: "lumea-machetelor-ab4ab",
+    storageBucket: "lumea-machetelor-ab4ab.appspot.com",
+    messagingSenderId: "817709164351",
+    appId: "1:817709164351:web:480c07abf20e613d84158a"
+  };
 
 export default firebaseConfig;
