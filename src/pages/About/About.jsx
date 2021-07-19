dc428b162b848a5b821f5d0c573938d135739735
andrei1311 <andrei.agrigoroaie@gmail.com>
2021-07-19 21:11:36 +0300

    new

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -17,17 +17,23 @@ function About() {
                             Bine ai venit pe site-ul meu!
                         </h3>
                         <p>
-                         Salut, eu sunt Andrei , am 32 de ani, iar 13 ani am lucrat in vanzari. Am urmat o facultate de Marketing si sigur acum te intrebi ce legatura au toate acestea web design-ul. <br></br>
-                            Cum am ajuns sa fac web-design?
-                        </p>
+                         Salut, eu sunt Andrei , am 32 de ani, iar 13 ani am lucrat in vanzari. Am urmat o facultate de Marketing si sigur acum te intrebi ce legatura au toate acestea web design-ul.
+                            
+                            <p><strong>Cum am ajuns sa fac web-design?</strong></p>
+                             
                         <p>
                             Acum 2 ani de zile, la sfaturile unui bun prieten care vedea un potential creativ in mine, am inceput cursurile de web design. <br></br>
-                            Astfel, am invatat HTML & CSS, ulterior am facut un curs de JavaScript Fundamentals, iar dupa finalizarea acestuia am inceput cursul de JavaScript Advanced - React. Asa am devenit din ce in ce mai pasionat de noile tendinte de design, tehnologii si solutii creative, toate acestea transformandu-se in noul meu hobby, pe care incerc din rasputeri sa-l dezvolt.
+                            Astfel, am invatat HTML & CSS, ulterior am facut un curs de JavaScript Fundamentals, iar dupa finalizarea acestuia am inceput cursul de JavaScript Advanced - React. <br></br>
+                            Asa am devenit din ce in ce mai pasionat de noile tendinte de design, tehnologii si solutii creative, toate acestea transformandu-se in noul meu hobby, pe care incerc din rasputeri sa-l dezvolt.
                         </p>
+
                         <p>
                             Acesta este primul meu "mare proiect" si cu siguranta nu ultimul, intrucat deja sunt multe pe lista care asteapta sa fie realizate in cel mai profesionist si atractiv mod posibil.
-                            <p>In cazul in care iti place lucrarea mea astept feedback-ul tau la adresa <strong><a href="mailto:andrei.agrigoroaie@gmail.com">andrei.agrigoroaie@gmail.com.</a></strong></p>
                         </p>
+
+                            <p>
+                                In cazul in care iti place lucrarea mea astept feedback-ul tau la adresa <strong><a href="mailto:andrei.agrigoroaie@gmail.com">andrei.agrigoroaie@gmail.com.</a></strong>
+                            </p>
                     </div>
                </div>
             </div>
